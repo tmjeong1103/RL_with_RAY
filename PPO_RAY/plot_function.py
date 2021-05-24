a12ca14c9fdd9d270f5e22ffed37ae8cbbebd4fd
tmjeong1103 <80529922+tmjeong1103@users.noreply.github.com>
2021-05-24 19:30:34 +0900

    Update plot_function.py

--- PPO_RAY/plot_function.py
+++ PPO_RAY/plot_function.py
@@ -14,20 +14,21 @@ def draw_subplt_graph(file_name1, file_name2):
         plot_dict1 = pickle.load(fp)
     with open('plot_data/%s.json'%file_name2,'rb') as fp2:
         plot_dict2 = pickle.load(fp2)
-
-    plt.subplots_adjust(hspace=0.5)
+    plt.figure(figsize=(10,8))
+    # plt.subplots_adjust(hspace=0.5)
     # make ep_ret-total step graph
     plt.subplot(2,1,1)
     plt.plot(list(plot_dict1.keys()),plot_dict1.values(),marker='o')
     plt.xlabel('step')
     plt.ylabel('ep_return')
-    plt.title("hdim:%s gamma:[%.4f] lam:[%.4f] clip_ratio:[%.4f]\n epsilon:[%.4f] ep_len_rollout:[%d]"
+    plt.title("hdim:%s gamma:[%.4f] lam:[%.4f] clip_ratio:[%.4f]\n epsilon:[%.4f] ep_len_rollout:[%d]\n"
               %(hdims,gamma,lam,clip_ratio,epsilon,ep_len_rollout))
     plt.grid(True, linestyle='--')
 
     plt.subplot(2,1,2)
     plt.plot(list(plot_dict2.keys()),plot_dict2.values(),marker='o')
     plt.xlabel('time')
+    plt.xticks(rotation=45)
     plt.ylabel('ep_return')
     # plt.title("hdim:%s gamma:[%.4f] lam:[%.4f] clip_ratio:[%.4f]\n epsilon:[%.4f] ep_len_rollout:[%d]"
     #           %(hdims,gamma,lam,clip_ratio,epsilon,ep_len_rollout))
